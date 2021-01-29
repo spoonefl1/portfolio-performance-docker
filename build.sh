0a0e1c8de2935310a84a59fde3a140b5299afc9b
spoonefl1 <spoonefl1@hotmail.com>
2021-01-29 17:24:15 -0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 
 set -euo pipefail
 
-VERSION=${VERSION:-0.48.1}
+VERSION=${VERSION:-0.50.3}
 CONTAINER=sunside/portfolio-performance:${VERSION}
 
 docker build --build-arg VERSION="${VERSION}" -t ${CONTAINER} .
